739ae574298a94e04da1bfd05480afcdebe97039
Peter Elliott <pelliott@ualberta.ca>
2019-06-22 09:50:05 -0700

    added TestCompileStmt

--- phdl/ast_test.go
+++ phdl/ast_test.go
@@ -71,3 +71,61 @@ func TestCompileBlock(t *testing.T) {
 		t.Error("stmt ret0 does not match block ret0")
 	}
 }
+
+func TestCompileStmt(t *testing.T) {
+	Parser := participle.MustBuild(
+		&Statement{},
+		participle.Lexer(Lexer),
+		participle.Elide("Whitespace", "OneLineComment", "MultiLineComment"),
+	)
+
+	prog1 := "(a)f -> a;"
+	prog2 := "(4)f -> a;"
+
+	ptree := &Statement{}
+	err := Parser.ParseString(prog1, ptree)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	astfile := &AstFile{Blocks: map[string]*AstBlock{
+		"f": &AstBlock{Name: "f"},
+		"tb": &AstBlock{Name: "tb", Vars: make(map[string]*AstConn)},
+	}}
+	ast, err := CompileStmt(astfile, astfile.Blocks["tb"], ptree)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := "(f [((a 0) 0 0)] [((a 0) 0 0)])"
+	if ast.String() != expected {
+		t.Errorf("expected/got:\n%s\n%s\n", expected, ast.String())
+	}
+
+	if ast.Op != astfile.Blocks["f"] {
+		t.Error("stmt block is not file block")
+	}
+
+	if ast.Args[0].Conn != ast.Rets[0].Conn || ast.Rets[0].Conn != astfile.Blocks["tb"].Vars["a"] {
+		t.Error("different sybmols for 'a'")
+	}
+
+	err = Parser.ParseString(prog2, ptree)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ast, err = CompileStmt(astfile, astfile.Blocks["tb"], ptree)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected = "(f [(4 0 0)] [((a 0) 0 0)])"
+	if ast.String() != expected {
+		t.Errorf("expected/got:\n%s\n%s\n", expected, ast.String())
+	}
+
+	if ast.Rets[0].Conn != astfile.Blocks["tb"].Vars["a"] {
+		t.Error("different sybmols for 'a'")
+	}
+}
